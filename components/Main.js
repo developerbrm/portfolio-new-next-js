ad937464fe70372614369f73907806355775548b
Braham Prakash <developer.brm@gmail.com>
2022-02-08 22:33:22 +0530

    - tailwind setup complete

---
+++ components/Main.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Main = () => {
+  return (
+    <main className="bg-slate-900 min-h-screen text-white">
+      main
+    </main>
+  );
+};
+
+export default Main;
---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: ["./pages/**/*.{html,js}", "./components/**/*.{html,js}"],
+  theme: {
+    screens: {
+      xs: "320px",
+      sm: "480px",
+      md: "768px",
+      lg: "976px",
+      xl: "1440px",
+    },
+  },
+  plugins: [],
+};
