6f130ee78e015c949b679c499e9668f33b2882f3
Braham Prakash <developer.brm@gmail.com>
2022-02-23 22:23:18 +0530

    - updated the tailwind config - properly extended the tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,26 +1,22 @@
-const colors = require("tailwindcss/colors");
 
 module.exports = {
   content: ["./pages/**/*.{html,js}", "./components/**/*.{html,js}"],
   theme: {
-    screens: {
-      xs: "320px",
-      sm: "480px",
-      md: "768px",
-      lg: "976px",
-      xl: "1440px",
-    },
-    fontFamily: {
-      laila: ["Laila", "sans-serif"],
-      quicksand: ["Quicksand", "sans-serif"],
-    },
-    colors: {
-      ...colors,
-      "custom-white-extra-light": "#F9F9F9",
-      "custom-white-light": "#E5E5E5",
-      "custom-yellow": "#FCA311",
-      "custom-dark-blue": "#14213D",
-      "custom-dark-black": "#111111",
+    extend: {
+      screens: {
+        xs: "320px",
+      },
+      fontFamily: {
+        laila: ["Laila", "sans-serif"],
+        quicksand: ["Quicksand", "sans-serif"],
+      },
+      colors: {
+        "custom-white-extra-light": "#F9F9F9",
+        "custom-white-light": "#E5E5E5",
+        "custom-yellow": "#FCA311",
+        "custom-dark-blue": "#14213D",
+        "custom-dark-black": "#111111",
+      },
     },
   },
   plugins: [],
